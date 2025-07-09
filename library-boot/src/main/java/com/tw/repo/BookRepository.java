bcb8896165337a8979ab36a025453b8e7132011e
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-07-09 15:53:58 +0530

    update and pretty print book Repository

--- library-boot/src/main/java/com/tw/repo/BookRepository.java
+++ library-boot/src/main/java/com/tw/repo/BookRepository.java
@@ -1,10 +1,8 @@
 package com.tw.repo;
 
-import com.tw.LibraryBootApplication;
 import com.tw.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
